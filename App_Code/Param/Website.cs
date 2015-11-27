f3c81daf1f473b30c069a18f995f216044ff7c11
彭逸豪 <hu60.cn@gmail.com>
2015-11-27 17:04:05 +0800

    更改数据库与目录结构

---
+++ App_Code/Param/Website.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Param
+{
+
+    /// <summary>
+    /// 网站参数
+    /// </summary>
+    public class Website
+    {
+        /// <summary>
+        /// 网站名称（用于页面显示）
+        /// </summary>
+        public const string SITE_NAME = "电信报表系统";
+
+        /// <summary>
+        /// 视图文件夹路径（用于页面跳转）
+        /// </summary>
+        public const string VIEW_PATH = "/View";
+    }
+}
--- View/Permission/Login.aspx
+++ View/Permission/Login.aspx
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>登录 - 电信报表系统</title>
+    <title>登录 - <%= Param.Website.SITE_NAME %></title>
 </head>
 <body>
     <form id="loginForm" runat="server">
