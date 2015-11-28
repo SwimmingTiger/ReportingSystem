cc7f7d2a682c67cb4e31fcc5c7f91a79f883029f
彭逸豪 <hu60.cn@gmail.com>
2015-11-28 16:30:57 +0800

    新增报表状态常量

---
+++ App_Code/Param/ReportStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Param
+{
+    /// <summary>
+    /// 报表数据状态
+    /// </summary>
+    public class ReportStatus
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        public const int NORMAL = 0;
+
+        /// <summary>
+        /// 未稽核
+        /// </summary>
+        public const int UNCHECKED = 1;
+
+        /// <summary>
+        /// 有错误
+        /// </summary>
+        public const int MISTAKE = 2;
+    }
+}
