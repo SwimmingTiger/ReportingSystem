a6d2761138a4044c5ade16eb2410602066a4792a
彭逸豪 <hu60@hu60.cn>
2015-11-29 12:07:37 +0800

    添加报表参数类型； 将报表状态类型改为枚举值。

---
+++ App_Code/Param/ReportParam.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Param
+{
+    /// <summary>
+    /// 报表参数管理
+    /// </summary>
+    public class ReportParam
+    {
+        /// <summary>
+        /// 参数类型
+        /// </summary>
+        public enum Type {
+            /// <summary>
+            /// 地市
+            /// </summary>
+            CITY = 0,
+
+            /// <summary>
+            /// 产品
+            /// </summary>
+            PRODUCT = 1,
+
+            /// <summary>
+            /// 出账收入类型
+            /// </summary>
+            ACCOUNT = 2,
+
+            /// <summary>
+            /// 销账类型
+            /// </summary>
+            CASH_RECONCILIATION = 3,
+
+            /// <summary>
+            /// 通知单收入类型
+            /// </summary>
+            NOTICE_LETTER = 4,
+
+            /// <summary>
+            /// 收款用途
+            /// </summary>
+            PAYMENT_PURPOSE = 5,
+
+            /// <summary>
+            /// 结算运营商
+            /// </summary>
+            OPERATOR = 6,
+
+            /// <summary>
+            /// 结算类型
+            /// </summary>
+            SEETLEMENT_TYPE = 7,
+        }
+
+        public enum Relation {
+            /// <summary>
+            /// 产品-出账对应关系
+            /// </summary>
+            PRODUCT_ACCOUNT = 0,
+
+            /// <summary>
+            /// 产品-通知单对应关系
+            /// </summary>
+            PRODUCT_NOTICE = 1,
+        }
+    }
+}
---
+++ App_Code/Param/ReportStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Param
+{
+    /// <summary>
+    /// 报表数据状态
+    /// </summary>
+    public enum ReportStatus
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        NORMAL = 0,
+
+        /// <summary>
+        /// 未稽核
+        /// </summary>
+        UNCHECKED = 1,
+
+        /// <summary>
+        /// 有错误
+        /// </summary>
+        MISTAKE = 2,
+    }
+}
